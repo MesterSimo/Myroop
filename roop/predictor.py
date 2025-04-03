6ec6d12bc4e197e3920c9e0586f498506b2649de
Khaoua Mohamed <132424419+MesterSimo@users.noreply.github.com>
2025-04-03 04:02:05 +0000

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,6 +1,5 @@
 import threading
 import numpy
-import opennsfw2
 from PIL import Image
 from keras import Model
 
@@ -16,28 +15,22 @@ def get_predictor() -> Model:
 
     with THREAD_LOCK:
         if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+            PREDICTOR = None  # NSFW model removed
     return PREDICTOR
 
 
 def clear_predictor() -> None:
     global PREDICTOR
-
     PREDICTOR = None
 
 
 def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
+    return False  # Always return False (no NSFW blocking)
 
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    return False  # Always return False (no NSFW blocking)
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    return False  # Always return False (no NSFW blocking)
